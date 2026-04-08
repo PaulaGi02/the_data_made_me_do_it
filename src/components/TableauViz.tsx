30188f637bb4fc8a34e80e880ca7ba97e26ae8db
PaulaGi02 <paula.giensch@stud.leuphana.de>
2026-04-08 13:09:16 +0800

    fixing tableau charts to be single handed

--- src/components/TableauViz.tsx
+++ src/components/TableauViz.tsx
@@ -8,10 +8,11 @@ export default function TableauViz({ vizUrl }: TableauVizProps) {
   return (
     <div className="w-full h-full flex items-center justify-center bg-white">
       <iframe
-        src={vizUrl}
-        allowFullScreen
-        style={{ width: '100%', height: '100%', border: 'none' }}
-      />
+  src={vizUrl}
+  allowFullScreen
+  style={{ width: '100%', height: '100%', border: 'none' }}
+  sandbox="allow-scripts allow-same-origin allow-forms allow-popups allow-top-navigation"
+/>
     </div>
   );
 }
